c6d8675892a24299c1740c038e74309a0fb52b7b
Gregory Orton <gregory.orton@workstream.is>
2026-01-21 21:36:19 +0800

    Initial Commit

---
+++ Tiller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tiller
+//
+//  Created by Gregory Orton on 2026/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tiller/TillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TillerApp.swift
+//  Tiller
+//
+//  Created by Gregory Orton on 2026/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
